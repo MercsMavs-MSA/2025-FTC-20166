c82e934f0d56827a8ba601ebd3b890c740c86f87
Jaxson West <jaxson.w@mckinneysteamacademy.org>
2026-09-15 20:21:33 -0500

    Copy main to 2026, update pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,58 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode.pedro;
 
 import com.pedropathing.follower.Follower;
-import com.pedropathing.follower.FollowerConstants;
-import com.pedropathing.ftc.FollowerBuilder;
-import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.constants.PinpointConstants;
-import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.RobotConstants;
-
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
-
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
-
-    private static MecanumConstants initializeMecanumConstants(RobotConstants robotConstants)
-    {
-        MecanumConstants driveConstants = new MecanumConstants()
-                .maxPower(1)
-                .rightFrontMotorName("FR")
-                .rightRearMotorName("BR")
-                .leftRearMotorName("BL")
-                .leftFrontMotorName("FL")
-                .leftFrontMotorDirection(robotConstants.leftSideDirection)
-                .leftRearMotorDirection(robotConstants.leftSideDirection)
-                .rightFrontMotorDirection(robotConstants.rightSideDirection)
-                .rightRearMotorDirection(robotConstants.rightSideDirection)
-                .useBrakeModeInTeleOp(false);
-        return driveConstants;
-    }
-
-    public static Follower createFollower(HardwareMap hardwareMap, RobotConstants robotConstants) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
-                .pinpointLocalizer(initializeLocalizerConstants(robotConstants))
-                .mecanumDrivetrain(initializeMecanumConstants(robotConstants))
-                .pathConstraints(pathConstraints)
-                .build();
-    }
-
-    private static PinpointConstants initializeLocalizerConstants(RobotConstants robotConstants)
-    {
-        PinpointConstants localizerConstants = new PinpointConstants()
-                .forwardPodY(robotConstants.podY)
-                .strafePodX(robotConstants.podX)
-                .distanceUnit(DistanceUnit.INCH)
-                .hardwareMapName("pinpoint")
-                .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
-                .forwardEncoderDirection(robotConstants.encoderForward)
-                .strafeEncoderDirection(robotConstants.encoderStrafe);
-        return localizerConstants;
+    public static Follower create(HardwareMap h) {
+        // return new Follower(Drivetrain, Localizer, Foresight);
+        return null;
     }
 }
